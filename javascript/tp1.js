1b7d24948d7eb35eb49215739fb3f691a379189e
matperez01 <144254480+matperez01@users.noreply.github.com>
2024-09-24 21:48:48 -0300

    consignas de trabajo

---
+++ javascript/tp1.js
@@ -0,0 +1,80 @@
+//? 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+// Dificultad:  🟢
+
+//? 2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+// Dificultad:  🟢
+
+//? 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+// Dificultad:  🟢
+
+//? 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+// Ejemplo: 
+
+// input: Coder 
+// Output: Hola Coder
+
+// Dificultad:  🟢
+
+//? 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+// Nota: Tener en cuenta la siguiente función: parseInt
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
+// Dificultad:  🟢
+
+//? Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+// Ejemplo: 
+
+// input: 15 , 3
+// Output: El 15 es el número más grande
+
+// Dificultad:  🟢
+
+//? 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+// Ejemplo: 
+
+// input: 15 , 3, 9
+// Output: El 15 es el número más grande
+
+// Dificultad:  🟢
+
+//? 8.- Escribe un programa que pida un número y diga si es divisible por 2
+// Ejemplo: 
+
+// input: 10
+// input: 15
+// Output: El 10 es divisible por 2.
+// Output: El 15 no es divisible por 2.
+
+
+
+// Dificultad:  🟢🟡 
+
+//? 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+// Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+// Ejemplo:
+
+
+// input: Hola mundo
+// Output: oauo
+
+// Dificultad:  🟢🟡
+
+//? 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+// Ejemplo: 
+
+// input: 20
+// Output: El 20 es divisible por 2.
+
+// Dificultad:  🟢🟡
+
+//? 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+// Ejemplo: 
+
+// input: 20
+// input: 210
+// Output: El 20 es divisible por 2 y por 5.
+// Output: El 210 es divisible por 2, por 3, por 5 y por 7.
+
+
+
+
+
