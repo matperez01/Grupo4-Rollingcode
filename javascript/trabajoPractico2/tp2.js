a5489c67f905518515f835397091fdae780f433e
matperez01 <144254480+matperez01@users.noreply.github.com>
2024-10-30 18:18:16 -0300

    se realizo el ejercicio 6 del tp2

--- javascript/trabajoPractico2/tp2.js
+++ javascript/trabajoPractico2/tp2.js
@@ -101,112 +101,112 @@ let respuesta = "";
 // Input:  40773821 
 // Output: ‘L’
 
-do {
-    let dni = parseInt(prompt(`ingrese un dni: `));
-    if (!isNaN(dni)) {
-       if (dni >= 0 && dni < 99999999) 
-           {
-           let valordiv = dni % 23
-           switch (valordiv) {
-               case 0:
-                  alert("la letra asignadada es: T");
-                    break;
+// do {
+//     let dni = parseInt(prompt(`ingrese un dni: `));
+//     if (!isNaN(dni)) {
+//        if (dni >= 0 && dni < 99999999) 
+//            {
+//            let valordiv = dni % 23
+//            switch (valordiv) {
+//                case 0:
+//                   alert("la letra asignadada es: T");
+//                     break;
 
-                case 1:
-                    alert("la letra asignadada es: R");
-                    break;
+//                 case 1:
+//                     alert("la letra asignadada es: R");
+//                     break;
 
-                case 2:
-                    alert("la letra asignadada es: W");
-                    break;
+//                 case 2:
+//                     alert("la letra asignadada es: W");
+//                     break;
 
-                case 3:
-                    alert("la letra asignadada es: A");
-                    break;
+//                 case 3:
+//                     alert("la letra asignadada es: A");
+//                     break;
 
-                case 4:
-                    alert("la letra asignadada es: G");
-                    break;
+//                 case 4:
+//                     alert("la letra asignadada es: G");
+//                     break;
 
-                case 5:
-                    alert("la letra asignadada es: M");
-                    break;
+//                 case 5:
+//                     alert("la letra asignadada es: M");
+//                     break;
 
-                case 6:
-                    alert("la letra asignadada es: Y");
-                    break;
+//                 case 6:
+//                     alert("la letra asignadada es: Y");
+//                     break;
 
-                case 7:
-                    alert("la letra asignadada es: F");
-                    break;
+//                 case 7:
+//                     alert("la letra asignadada es: F");
+//                     break;
 
-                case 8:
-                    alert("la letra asignadada es: P");
-                    break;
+//                 case 8:
+//                     alert("la letra asignadada es: P");
+//                     break;
 
-                case 9:
-                    alert("la letra asignadada es: D");
-                    break;
+//                 case 9:
+//                     alert("la letra asignadada es: D");
+//                     break;
 
-                case 10:
-                    alert("la letra asignadada es: X");
-                    break;
+//                 case 10:
+//                     alert("la letra asignadada es: X");
+//                     break;
 
-                case 11:
-                    alert("la letra asignadada es: B");
-                    break;
+//                 case 11:
+//                     alert("la letra asignadada es: B");
+//                     break;
 
-                case 12:
-                    alert("la letra asignadada es: N");
-                    break;
+//                 case 12:
+//                     alert("la letra asignadada es: N");
+//                     break;
 
-                case 13:
-                    alert("la letra asignadada es: J");
-                    break;
+//                 case 13:
+//                     alert("la letra asignadada es: J");
+//                     break;
 
-                case 14:
-                    alert("la letra asignadada es: Z");
-                    break;
+//                 case 14:
+//                     alert("la letra asignadada es: Z");
+//                     break;
 
-                case 15:
-                    alert("la letra asignadada es: S");
-                    break;
+//                 case 15:
+//                     alert("la letra asignadada es: S");
+//                     break;
 
-                case 16:
-                    alert("la letra asignadada es: Q");
-                    break;
+//                 case 16:
+//                     alert("la letra asignadada es: Q");
+//                     break;
 
-                case 17:
-                    alert("la letra asignadada es: V");
-                    break;
+//                 case 17:
+//                     alert("la letra asignadada es: V");
+//                     break;
 
-                case 18:
-                    alert("la letra asignadada es: H");
-                    break;
+//                 case 18:
+//                     alert("la letra asignadada es: H");
+//                     break;
 
-                case 19:
-                    alert("la letra asignadada es: L");
-                    break;
-                case 20:
-                    alert("la letra asignadada es: C");
-                    break;
+//                 case 19:
+//                     alert("la letra asignadada es: L");
+//                     break;
+//                 case 20:
+//                     alert("la letra asignadada es: C");
+//                     break;
 
-                case 21:
-                    alert("la letra asignadada es: K");
-                    break;
+//                 case 21:
+//                     alert("la letra asignadada es: K");
+//                     break;
 
-                case 22:
-                    alert("la letra asignadada es: E");
-                    break;
+//                 case 22:
+//                     alert("la letra asignadada es: E");
+//                     break;
 
-                default:
-                 alert("Número erróneo"); 
+//                 default:
+//                  alert("Número erróneo"); 
 
-            }
+//             }
 
-        }else{alert("ingrese cantidad de numeros validos ")} 
-    }
-} while (confirm ("¿Desea continuar?"));
+//         }else{alert("ingrese cantidad de numeros validos ")} 
+//     }
+// } while (confirm ("¿Desea continuar?"));
 
 
 
@@ -227,19 +227,15 @@ do {
 // 55555
 // 666666
 
-// for (let i=1; i <= 30; i++)
-// {
-//     for (let j=0 ; j < i ; j++)
-//         document.write(i)
-//         document.write("<br>")
-// }
-    
-
-
-
-
-
-
+for (let i=1; i <= 30; i++)
+{
+ for (let j=0 ; j < i ; j++)
+ {
+    document.write(i)
+    document.write("<br>")
+ }
+           
+}
 // 7- Haz un script que escriba una pirámide inversa de los números del 1 al número que indique el usuario (no mayor de 50)  de la siguiente forma : (suponiendo que indica 30).
 
 // 303030303030303030303030303030303030303030303030303030303030
