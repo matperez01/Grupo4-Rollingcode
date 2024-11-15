f9ecc8c5d026f7ca5614cd89c2547d767c91d7f6
jose maria perez <josema.perez.020@gmail.com>
2024-11-15 19:59:07 -0300

    hola

--- javascript/trabajoPractico2/tp2.js
+++ javascript/trabajoPractico2/tp2.js
@@ -316,6 +316,7 @@ for (let i = 1; i <= num.length; i++) {
 
 // Dificultad:  🟢🟡🔴
 // 10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+let filas = prompt("ingrese cantidad de fila");
 
 // Ejercicios con Math
 
